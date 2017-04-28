4637ec5db6f021619719a82e533f412f32756fb4
Larisa Dolgova <larisa.dolgova@gmail.com>
2017-04-28 22:57:20 +0200

    Implement counter

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ const gallerySize = 5;
 
 function addImage() {
   const img = document.querySelector('.image');
+  const counterDiv = document.querySelector('.counter');
   img.setAttribute("src", "http://lorempixel.com/600/350/?" + counter);
-  document.querySelector('.image-container').appendChild(img)
+  document.querySelector('.image-container').appendChild(img);
+  counterDiv.innerHTML = counter + " / " + gallerySize
   console.log(img.getAttribute("src"))
 }
 
 function changeImage() {
   const img = document.querySelector('.image');
+  const counterDiv = document.querySelector('.counter');
   img.setAttribute("src", "http://lorempixel.com/600/350/?" + counter);
+  counterDiv.innerHTML = counter + " / " + gallerySize
   console.log(img.getAttribute("src"))
 }
 
